3658171beb4d61652a0aa6d77d6edd63286d69d0
nakul <nakulgunjal9@gmail.com>
2023-03-05 19:31:03 +0530

    funcitons in typescript

--- 1-intro/myfunction.js
+++ 1-intro/myfunction.js
@@ -3,4 +3,10 @@ exports.__esModule = true;
 function addTwo(num) {
     return num + 2;
 }
-addTwo("5");
+addTwo(8);
+function getUpper(val) {
+    return val.toUpperCase();
+}
+getUpper("hello");
+function signUpUser(name, email, password) { }
+signUpUser("hi", "dll", "password");
--- 1-intro/myfunction.ts
+++ 1-intro/myfunction.ts
@@ -9,10 +9,45 @@ function getUpper(val: string) {
 }
 getUpper("hello");
 
-function signUpUser(name: string , email: string, password: string) {
+function signUpUser(name: string, email: string, password: string) {}
 
+signUpUser("hi", "dll", "password");
+
+//to specify to get a specify a return type
+
+function addTwwo(num: number): number {
+  return num + 2;
 }
 
-signUpUser(1,3,3){
+// this type of senario can happen when a return type can be both a string or a boolean
 
+function getValue(val: number): string {
+  if (val > 5) {
+    return true;
+  }
+  return "200 OK";
+}
+
+const getHello = (s: string): string => {
+  return "Hello";
 };
+
+// here is typescript understand that our array is string so it know that the parameters are string
+
+const heros = ["thor", "spiderman", "ironman"];
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+// consoleError (return void i.e. returns nothing)
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+}
+
+//never (never returns anything not even void)
+
+function handleError(val: string): never {
+  throw new Error(val);
+}
+
+export {};

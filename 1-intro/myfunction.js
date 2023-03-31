d3274bd2d8218fbd1064fa9859d97f795dac1fd0
nakul <nakulgunjal9@gmail.com>
2023-03-31 17:53:47 +0530

    myfuntion

--- 1-intro/myfunction.js
+++ 1-intro/myfunction.js
@@ -10,3 +10,30 @@ function getUpper(val) {
 getUpper("hello");
 function signUpUser(name, email, password) { }
 signUpUser("hi", "dll", "password");
+//to specify to get a specify a return type
+function addTwwo(num) {
+    return num + 2;
+}
+// this type of senario can happen when a return type can be both a string or a boolean
+function getValue(val) {
+    if (val > 5) {
+        return true;
+    }
+    return "200 OK";
+}
+var getHello = function (s) {
+    return "Hello";
+};
+// here is typescript understand that our array is string so it know that the parameters are string
+var heros = ["thor", "spiderman", "ironman"];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+// consoleError (return void i.e. returns nothing)
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+//never (never returns anything not even void)
+function handleError(val) {
+    throw new Error(val);
+}
